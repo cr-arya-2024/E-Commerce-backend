7d0039cc08988157249c101f95c78a4a937bf7ea
cr-arya-2024 <crarya2024@gmail.com>
2025-08-27 13:55:17 +0530

    added the basic structure needed for the backend part

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+
+const app=express()
+const port=process.env.PORT || 8000
+connectDB()
+app.listen(port,()=>{
+    console.log('server is running on 8000');
+      
+    
+})

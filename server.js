b141cb5c49348f5075ee94e1922657117c30623b
cr-arya-2024 <crarya2024@gmail.com>
2025-08-29 00:06:45 +0530

    cloudinary added

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 const app=express()
 const port=process.env.PORT || 8000
 connectDB()
+connectCloudinary()
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send("API WORKING")
+})
+
+
 app.listen(port,()=>{
-    console.log('server is running on 8000');
+    console.log(`server is running on ${port} `);
 })

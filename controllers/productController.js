06f6fd431e6dba54712f1b7f93ba032a1476d9c3
cr-arya-2024 <crarya2024@gmail.com>
2025-09-01 00:09:42 +0530

    added some essential add new product functionalities

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,55 @@
+import {v2 as cloudinary} from 'cloudinary'
+import productModel from '../models/productModel.js'
 //function for add product
 
 const addProduct=async(req,res)=>{
-    
+    try {
+        const {name,description,price,category,subCategory,sizes,bestseller}=req.body
+        const image1=req.files.image1 && req.files.image1[0]
+        const image2=req.files.image2 && req.files.image2[0]
+        const image3=req.files.image3 && req.files.image3[0]
+        const image4=req.files.image4 && req.files.image4[0]
+
+        const images=[image1,image2,image3,image4].filter((item)=>item!==undefined)
+
+        console.log(name,description,price,category,subCategory,sizes,bestseller);
+      
+        //this part explains about url created by cloudinary to store in cloudinary
+        let imagesUrl=await Promise.all(
+            images.map(async(image)=>{
+                // Upload image to cloudinary
+                const result=await cloudinary.uploader.upload(image.path,{
+                    resource_type:"image"
+                }) 
+                return result.secure_url
+            })
+        )
+        console.log(imagesUrl);
+        const productData={
+            name,
+            description,
+            price:parseInt(price),
+            category,
+            subCategory,
+            sizes:sizes.split(','),
+            bestseller:bestseller==="true"?true:false,
+            image:imagesUrl,
+            date:Date.now()//this part is neccesary 
+        }
+        console.log(productData);
+        const product=new productModel(productData)
+        await product.save()
+console.log(product);
+
+        res.json({success:true,message:"Product added successfully"})
+        
+        
+    } catch (error) {
+        console.log(error);
+        
+        res.json({success:false,message:error.message})
+    }
+
 }
 const listProduct=async(req,res)=>{
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -35,14 +35,14 @@ const registerUser=async(req,res)=>{
             return res.json({success:false,message:"User already exists"})
         }
         if(!validator.isEmail(email)){return res.json({success:false,message:"Plz enter a vaild email"})}
-        if(password.lenght<  8){return res.json({success:false,message:"Plz enter a strong password"})}
+        if(password.length < 8){return res.json({success:false,message:"Plz enter a strong password"})}
 const salt=await bcrypt.genSalt(10)
 const hashedPassword=await bcrypt.hash(password,salt)
 
 const newUser=new userModel({name,email,password:hashedPassword})
 const user=await newUser.save()
 const token=creatToken(user._id)
-res.json({success:"true",token})
+res.json({success:true,token})
     } catch (error) {
        console.log(error);
        res.json({success:false,message:error.message})
